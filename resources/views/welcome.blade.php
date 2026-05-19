515e28a83f4d0ca274fe1b9e2f26ba9b743de717
Eduardo Coello <eduardongua@hotmail.com>
2026-05-19 15:54:08 -0700

    sistema diseño scss. colores y tipografias añadidas.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pulse_meter</title>
     
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/styles/main.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div
