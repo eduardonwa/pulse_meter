fd38d28f92da4756dede61f6ab6e392fd12c4c8f
Eduardo Coello <eduardongua@hotmail.com>
2026-05-20 00:38:20 -0700

    x-cloak en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pulse</title>
     @vite(['resources/styles/main.scss', 'resources/js/app.js'])
+    <style> [x-cloak] { display: none !important; } </style>
 </head>
 <body>
 
