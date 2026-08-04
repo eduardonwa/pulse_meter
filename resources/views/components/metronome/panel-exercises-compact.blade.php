06bacf11ab85f451609d804ad9a5cfd8ff7fcc8f
Eduardo Coello <eduardongua@hotmail.com>
2026-08-03 21:47:53 -0700

    diseño para modo playlist puesto

--- resources/views/components/metronome/panel-exercises-compact.blade.php
+++ resources/views/components/metronome/panel-exercises-compact.blade.php
@@ -71,32 +71,8 @@
                     :class="{ 'is-active': isPlaying && activeSessionType === 'exercise' && currentIndex === index }"
                     @click.stop="activeExerciseIndex === index && isPlaying ? stop() : startExercise(index)"
                 >
-                    <!-- Pause icon -->
-                    <svg
-                        x-show="activeExerciseIndex === index && isPlaying"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="pause-icon"
-                    >
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9v6m-4.5 0V9M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-
-                    <!-- Play icon -->
-                    <svg
-                        x-show="!(activeExerciseIndex === index && isPlaying)"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="play-icon"
-                    >
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z" />
-                    </svg>
+                    <x-heroicon-o-pause-circle  class="pause-icon" x-show="activeExerciseIndex === index && isPlaying" />
+                    <x-heroicon-o-play-circle class="play-icon" x-show="!(activeExerciseIndex === index && isPlaying)" />
                 </button>
             </section>
         </div>
--- resources/views/components/metronome/panel-playlist-groups.blade.php
+++ resources/views/components/metronome/panel-playlist-groups.blade.php
@@ -1,95 +1,56 @@
 <div class="playlist-practice">
     <template x-if="practiceGroups.length === 0">
         <p class="playlist-practice__empty">
-            This playlist has no routines yet.
+            This playlist is empty. Add a routine to start practicing.
         </p>
     </template>
 
-    <template
-        x-for="group in practiceGroups"
-        :key="
-            group.is_starter
-                ? 'starter-' + group.routine_id
-                : 'item-' + group.playlist_item_id
-        "
-    >
+    <template x-for="group in practiceGroups" :key="group.is_starter ? 'starter-' + group.routine_id : 'item-' + group.playlist_item_id">
         <section class="playlist-practice__group">
             <header class="playlist-practice__heading">
                 <h3 x-text="group.routine_name"></h3>
 
-                <span
-                    class="badge"
-                    x-show="group.is_starter"
-                >
+                <span class="badge badge--starter-routine" x-show="group.is_starter">
                     Starter Routine
                 </span>
             </header>
 
             <ul class="playlist-practice__exercises">
-                <template
-                    x-for="step in group.exercises"
-                    :key="
-                        group.routine_id
-                        + '-'
-                        + step.exercise_id
-                    "
-                >
-                    <li
-                        class="exercise-layout"
+                <template x-for="step in group.exercises" :key="group.routine_id + '-' + step.exercise_id">
+                    <li class="exercise-layout"
                         :class="{
                             'is-active':
                                 isPlaying
-                                && activeSessionType === 'exercise'
-                                && currentIndex === step.queue_position
+                                    && activeSessionType === 'exercise'
+                                    && currentIndex === step.queue_position
                         }"
                     >
                         <div class="exercise-layout__bpm">
-                            <span
-                                class="exercise-row__bpm"
-                                x-text="step.bpm"
-                            ></span>
+                            <span class="exercise-row__bpm" x-text="step.bpm"></span>
                         </div>
 
                         <div class="exercise-layout__info">
                             <section class="summary">
-                                <input
-                                    class="name"
-                                    type="text"
-                                    :value="step.name"
-                                    readonly
-                                >
+                                <p class="exercise-name" x-text="step.name"></p>
 
-                                <div class="bottom">
-                                    <span
-                                        class="timer"
-                                        :class="{
-                                            'is-counting':
-                                                isPlaying
+                                <span class="timer"
+                                    :class="{
+                                        'is-counting':
+                                            isPlaying
                                                 && activeSessionType === 'exercise'
                                                 && currentIndex === step.queue_position
                                                 && step.mode === 'timer',
 
-                                            'badge':
-                                                step.mode === 'classic'
-                                        }"
-                                        x-text="
-                                            getStepTimeLabel(
-                                                step,
-                                                step.queue_position
-                                            )
-                                        "
-                                    ></span>
-                                </div>
+                                        'badge': step.mode === 'classic'
+                                    }"
+                                    x-text="getStepTimeLabel(step, step.queue_position)"
+                                ></span>
                             </section>
 
                             <section class="play-exercise">
-                                <button
-                                    type="button"
-                                    class="exercise-row__playback | button"
-                                    data-type="action-exercise"
+                                <button class="exercise-row__playback | button" type="button" data-type="action-exercise"
                                     :class="{
-                                        'is-active':
-                                            isPlaying
+                                        'is-active': isPlaying
                                             && activeSessionType === 'exercise'
                                             && currentIndex === step.queue_position
                                     }"
@@ -97,56 +58,11 @@
                                         activeExerciseIndex === step.queue_position
                                         && isPlaying
                                             ? stop()
-                                            : startExercise(
-                                                step.queue_position
-                                            )
+                                            : startExercise(step.queue_position)
                                     "
                                 >
-                                    <svg
-                                        x-show="
-                                            activeExerciseIndex === step.queue_position
-                                            && isPlaying
-                                        "
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke-width="1.5"
-                                        stroke="currentColor"
-                                        class="pause-icon"
-                                    >
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            d="M14.25 9v6m-4.5 0V9M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                                        />
-                                    </svg>
-
-                                    <svg
-                                        x-show="
-                                            !(
-                                                activeExerciseIndex === step.queue_position
-                                                && isPlaying
-                                            )
-                                        "
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke-width="1.5"
-                                        stroke="currentColor"
-                                        class="play-icon"
-                                    >
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                                        />
-
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
-                                        />
-                                    </svg>
+                                    <x-heroicon-o-pause-circle class="pause-icon" x-show="activeExerciseIndex === step.queue_position && isPlaying" />
+                                    <x-heroicon-o-play-circle class="play-icon" x-show="!(activeExerciseIndex === step.queue_position && isPlaying)"/>
                                 </button>
                             </section>
                         </div>
