32d6203fcdf587579def34ec8113c4df3e72794e
Eduardo Coello <eduardongua@hotmail.com>
2026-08-05 15:24:33 -0700

    config(billing): define Stripe Pro prices

---
+++ config/billing.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'pro' => [
+        'monthly_price_id' => env(
+            'DORELOG_PRO_MONTHLY_PRICE_ID'
+        ),
+
+        'lifetime_price_id' => env(
+            'DORELOG_PRO_LIFETIME_PRICE_ID'
+        ),
+    ],
+];
