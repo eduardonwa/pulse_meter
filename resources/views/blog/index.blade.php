83fa4b6e69a054585a67427343c3090b59c4d2bc
Eduardo Coello <eduardongua@hotmail.com>
2026-08-18 11:12:49 -0700

    barra de idiomas en blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,6 +1,30 @@
 <x-layouts.dorelog>
     <section class="container | posts" data-type="blog-post">
         <div class="flow">
+            <div class="lang" x-data="{ dropdownOpen: false }">
+
+                <button type="button" class="button" data-type="icon-text"
+                    @click="dropdownOpen = ! dropdownOpen"
+                    @keydown.escape.window="dropdownOpen = false"
+                    @click.outside="dropdownOpen = false"
+                >
+                    <x-heroicon-o-language />
+                    Language:
+                    {{ app()->getLocale() === 'es' ? 'Español' : 'English' }}
+                    <x-heroicon-m-chevron-down />
+                </button>
+
+                <div class="lang__options" x-show="dropdownOpen === true" x-cloak>
+                    <a class="option" href="{{ route('blog.index', ['locale' => 'en']) }}">
+                        English
+                    </a>
+    
+                    <a class="option" href="{{ route('blog.index', ['locale' => 'es']) }}">
+                        Español
+                    </a>
+                </div>
+            </div>
+
             <h1 class="heading-3">Dorelog Blog</h1>
             <p class="fs-300">
                 Useful ideas, practical examples, and the occasional recommendation. Open to relevant partnerships and sponsorships. <a href="/contact">Contact me</a>.
