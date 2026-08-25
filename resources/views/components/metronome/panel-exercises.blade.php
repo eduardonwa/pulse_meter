6eed1b7c224ee882c6714f39bb2b800893a86535
Eduardo Coello <eduardongua@hotmail.com>
2026-08-24 21:45:27 -0700

    cambio de posicion de boton. color en badge. tabla de contenidos es sitcky.

--- resources/views/components/metronome/panel-exercises.blade.php
+++ resources/views/components/metronome/panel-exercises.blade.php
@@ -17,15 +17,6 @@
                     <x-heroicon-o-arrow-top-right-on-square />
                 </button>
             @endif
-
-            <button class="add-exercise | button" data-type="icon-text" type="button"
-                x-show="canManageExercises"
-                :disabled="steps.length >= maxSteps"
-                @click="openAddStepModal()"
-            >
-                <x-heroicon-o-plus-circle />
-                Add
-            </button>
         </header>
 
         <template x-if="practiceMode === 'routine'">
@@ -51,6 +42,17 @@
         <x-windows.advance-modal />
 
         <x-windows.practice-review-modal />
+
+        <div class="footer-bar">
+            <button class="add-exercise | button" data-type="icon-text" type="button"
+                x-show="canManageExercises"
+                :disabled="steps.length >= maxSteps"
+                @click="openAddStepModal()"
+            >
+                <x-heroicon-o-plus-circle />
+                Add Exercise
+            </button>
+        </div>
 
         <p class="total-exercises" x-show="canManageExercises">
             <span x-text="steps.length"></span>
