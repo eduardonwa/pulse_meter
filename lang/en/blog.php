8d005f2d5b458103980f3808b403c49d5fb48417
Eduardo <40727355+eduardonwa@users.noreply.github.com>
2026-08-27 12:35:28 -0700

    Add CTA variants: blog.php

--- lang/en/blog.php
+++ lang/en/blog.php
@@ -8,15 +8,24 @@ return [
     'contact_link' => 'Contact me.',
 
     'cta' => [
-        'heading' => 'Take control of your practice',
-        'button_label' => 'Get started',
-        'benefits' => <<<'HTML'
-            <ul>
-                <li>Create routines with your own exercises.</li>
-                <li>Practice exercises with tablature.</li>
-                <li>Use the Pulse Editor to discover and practice rhythmic patterns.</li>
-                <li>Organize up to 20 exercises per routine and combine routines into playlists.</li>
-            </ul>
-        HTML,
+        'subscription' => [
+            'heading' => 'Take control of your practice',
+            'button_label' => 'Get started',
+            'benefits' => <<<'HTML'
+                <ul>
+                    <li>Create routines with your own exercises.</li>
+                    <li>Practice exercises with tablature.</li>
+                    <li>Use the Pulse Editor to discover and practice rhythmic patterns.</li>
+                    <li>Organize up to 20 exercises per routine and combine routines into playlists.</li>
+                </ul>
+            HTML,
+        ],
+        'resource' => [
+            'eyebrow' => 'Recommended resource',
+            'heading' => 'Practice with this resource',
+            'description' => 'Open the resource related to this article and put what you just learned into practice.',
+            'button_label' => 'Open resource',
+            'supporting_text' => '',
+        ],
     ],
 ];
