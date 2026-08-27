5bbd47b4eae2184d2e0e80cb70b08558132ed322
Eduardo <40727355+eduardonwa@users.noreply.github.com>
2026-08-27 12:35:48 -0700

    Add CTA variants: index.blade.php

--- resources/views/filament/forms/components/rich-editor/rich-content-custom-blocks/call-to-action/index.blade.php
+++ resources/views/filament/forms/components/rich-editor/rich-content-custom-blocks/call-to-action/index.blade.php
@@ -1,15 +1,66 @@
-<aside class="post--cta">
+@php
+    $type = $type ?? 'subscription';
+    $buttonColor = $button_color ?? 'dark';
+    $stats = array_filter([
+        $stat_1 ?? null,
+        $stat_2 ?? null,
+        $stat_3 ?? null,
+    ]);
+@endphp
+
+<aside
+    class="post--cta"
+    data-cta-type="{{ $type }}"
+    data-has-media="{{ $coverImageUrl ? 'true' : 'false' }}"
+>
+    @if ($type === 'resource' && $coverImageUrl)
+        <div class="post--cta__media">
+            <img
+                class="post--cta__cover"
+                src="{{ $coverImageUrl }}"
+                alt="{{ $cover_alt ?? '' }}"
+                loading="lazy"
+            >
+        </div>
+    @endif
+
     <div class="post--cta__content">
+        @if ($type === 'resource' && filled($eyebrow ?? null))
+            <p class="post--cta__eyebrow">{{ $eyebrow }}</p>
+        @endif
+
         <h2 class="post--cta__heading">
             {{ $heading }}
         </h2>
 
-        <div class="post--cta__benefits">
-            {!! str($benefits)->sanitizeHtml() !!}
-        </div>
+        @if ($type === 'resource')
+            <p class="post--cta__description">{{ $description }}</p>
 
-        <a class="button" data-type="accent" href="{{ $button_url }}">
+            @if ($stats !== [])
+                <ul class="post--cta__stats">
+                    @foreach ($stats as $stat)
+                        <li>{{ $stat }}</li>
+                    @endforeach
+                </ul>
+            @endif
+        @else
+            <div class="post--cta__benefits">
+                {!! str($benefits ?? '')->sanitizeHtml() !!}
+            </div>
+        @endif
+
+        <a
+            class="button post--cta__button"
+            data-button-color="{{ $buttonColor }}"
+            href="{{ $button_url }}"
+        >
             {{ $button_label }}
         </a>
+
+        @if ($type === 'resource' && filled($supporting_text ?? null))
+            <p class="post--cta__supporting-text">
+                {{ $supporting_text }}
+            </p>
+        @endif
     </div>
 </aside>
